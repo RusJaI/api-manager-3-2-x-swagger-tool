3444ff611d7af6f7d6ccad819499ef82255fb8af
rusirijayodaillesinghe <rusiri@wso2.com>
2024-01-08 11:52:35 +0530

    Add license headers

--- src/main/java/org/wso2/apim/swagger/tool/Constants.java
+++ src/main/java/org/wso2/apim/swagger/tool/Constants.java
@@ -1,7 +1,26 @@
+/*
+ * Copyright (c) 2024, WSO2 LLC. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.wso2.apim.swagger.tool;
 
 public class Constants {
-    public static final String OPENAPI_VERSION_NOT_FOUND_ERROR_CODE = "OpenAPI version Not found or invalid version found in the definition";
+    public static final String OPENAPI_VERSION_NOT_FOUND_ERROR_CODE =
+            "OpenAPI version Not found or invalid version found in the definition";
     public static final String OPENAPI_NAME_NOT_FOUND_ERROR_CODE = "OpenAPI name Not found in the definition";
     public static final String OPENAPI_PARSE_EXCEPTION_ERROR_MESSAGE = "Error while parsing OpenAPI definition";
     public static final String UNABLE_TO_RENDER_THE_DEFINITION_ERROR = "Unable to render this definition, " +
--- src/main/java/org/wso2/apim/swagger/tool/SwaggerValidateUtils.java
+++ src/main/java/org/wso2/apim/swagger/tool/SwaggerValidateUtils.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2024, WSO2 LLC. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.wso2.apim.swagger.tool;
 
 import com.fasterxml.jackson.databind.JsonNode;
@@ -151,7 +169,7 @@ public class SwaggerValidateUtils {
         if (parseAttemptForV2.getMessages().size() > 0) {
 
             for (String message : parseAttemptForV2.getMessages()) {
-                StringBuilder errorMessageBuilder = new StringBuilder("Invalid Swagger, Error Code: ");
+                StringBuilder errorMessageBuilder = new StringBuilder("Invalid Swagger");
 
                 // Since OpenAPIParser coverts the $ref to #/components/schemas/ when validating
                 // we need to replace #/components/schemas/ with #/definitions/ before printing the message
@@ -162,7 +180,7 @@ public class SwaggerValidateUtils {
                 errorMessageBuilder.append(", Error: ").append(message);
 
                 // write to file
-                writeResults(fileWriter, null, null, null, message);
+                writeResults(fileWriter, null, null, null, errorMessageBuilder.toString());
 
                 log.error(errorMessageBuilder.toString());
             }
@@ -206,8 +224,7 @@ public class SwaggerValidateUtils {
 
             for (String message : parseResult.getMessages()) {
 
-                StringBuilder errorMessageBuilder = new StringBuilder("Invalid OpenAPI, Error Code: ");
-                errorMessageBuilder.append(", Error: ").append(message);
+                StringBuilder errorMessageBuilder = new StringBuilder("Invalid OpenAPI");
 
                 // If the error message contains "schema is unexpected", we modify the error message notifying
                 // that the schema object is not adhering to the OpenAPI Specification. Also, we add a note to
@@ -221,7 +238,7 @@ public class SwaggerValidateUtils {
                         .append(", Swagger Error: ").append(message);
 
                 // write to file
-                writeResults(fileWriter, null, null, null, message);
+                writeResults(fileWriter, null, null, null, errorMessageBuilder.toString());
 
                 log.error(errorMessageBuilder.toString());
             }
