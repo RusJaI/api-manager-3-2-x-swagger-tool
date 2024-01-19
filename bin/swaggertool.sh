feae463e8e40461343e943892f41aa2b9acec02b
rusirijayodaillesinghe <rusiri@wso2.com>
2024-01-19 15:09:57 +0530

    Pack Jcommander dependency

--- bin/swaggertool.sh
+++ bin/swaggertool.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/sh
 # ----------------------------------------------------------------------------
 
 #Copyright (c) 2024, WSO2 LLC. (http://www.wso2.org) All Rights Reserved.
@@ -23,11 +23,12 @@
 jar_path="./api-manager-3-2-x-swagger-tool-1.0-0.jar"
 
 # Specify the classpath (replace with your actual classpath)
-current_directory=$(pwd)
+current_directory=${PWD%/*}
+
+echo $current_directory
+
 custom_classpath="$jar_path"
 
-string1='com.fasterxml.jackson.*'
-string='org.wso2.carbon.authenticator.stub*';
 declare -a arr=("com.fasterxml.jackson" "org.wso2.carbon.authenticator.stub" "org.wso2.carbon.registry.resource.stub" "jcommander" "commons-logging" "axis2" "org.wso2.carbon.um.ws.api.stub" "axiom_" "wsdl4j.wso2" "XmlSchema" "neethi" "activation" "org.wso2.securevault" "commons-httpclient" "httpclient_" "httpcore_" "commons-codec-" "swagger-parser_" "slf4j-api" "org.yaml.snakeyaml" "swagger-core" "org.wso2.carbon.utils_" "org.wso2.carbon.base_")
 
 for t in "$current_directory"/repository/components/plugins/*.jar
@@ -55,7 +56,7 @@ echo "$custom_classpath"
 echo "\n"
 
 # Execute the Java command with options -u and -d
-java  -Dlog4j2.configurationFile=/Users/rusirijayodaillesinghe/Documents/patch/3.2.0/Test_swagger_tool/wso2am-3.2.0/log4j2.xml -cp "$custom_classpath" org.wso2.apim.swagger.tool.SwaggerTool  "$@"
+java -cp "$custom_classpath" org.wso2.apim.swagger.tool.SwaggerTool  "$@"
 
 # java -cp /Users/abc/Documents/APIM_Repos/api-manager-3-2-x-swagger-tool/lib/ -jar apim-swagger-validator-1.0-0.jar
 
